d6fbff72b7d9a09331d1dbeaa394d37e35afca32
John Gorriceta <79299314+jongee0w0@users.noreply.github.com>
2023-09-21 03:41:50 -0600

    boilerplate pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+	registerType: "prompt",
+	includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: "./",
+	plugins: [react(), VitePWA(manifestForPlugin)],
+});
