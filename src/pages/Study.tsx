86eb3eaaf46f483c68a812c287e9092f5eb1af4b
John Gorriceta <79299314+jongee0w0@users.noreply.github.com>
2023-09-23 14:30:02 -0600

    added not func

--- src/pages/Study.tsx
+++ src/pages/Study.tsx
@@ -1,10 +1,15 @@
 import {useEffect, useState} from 'react'
 import { Verse } from '../lib/verse'
 import { Drawer } from 'vaul'
-import { AiOutlineInfoCircle, AiOutlineShareAlt, AiOutlineEdit} from "react-icons/ai";
+import { AiOutlineInfoCircle, AiOutlineShareAlt, AiOutlineEdit, AiOutlineDownload} from "react-icons/ai";
+import { JellyTriangle } from '@uiball/loaders'
+import { noteAtom } from '../lib/noteAtom';
+import { useAtom } from 'jotai';
 
 export default function Study(){
   const [scripture, setScripture] = useState <Verse|null>(null)
+  const [note, setNote] = useAtom(noteAtom);
+  const [downloadURL, setDownloadURL] = useState<Blob|null>(null);
   useEffect(()=>{
     getScriptures()
   },[])
@@ -18,6 +23,8 @@ export default function Study(){
     return `https://www.churchofjesuschrist.org/study/scriptures/bofm/${title}/${chapter}?lang=eng&id=p${verse}#p${verse}`
   }
 
+  let currDate = (new Date()).toJSON().slice(0, 10)
+
   // url to open scriptures to specific verse
   //https://www.churchofjesuschrist.org/study/scriptures/bofm/2-ne/2?lang=eng&id=p11#p11
 
@@ -25,10 +32,10 @@ export default function Study(){
   const shareNotes = () => {
     if (navigator.share) {
       navigator.share({
-        title: document.title,
-        text: "Hello World",
+        title: "Celestial",
+        text: `${currDate} Notes`,
         files: [
-          new File([blobby], 'test.txt', {
+          new File([downloadURL?downloadURL:"/"], `Celestial-${currDate}.txt`, {
             type: "text/plain",
           })
         ]
@@ -41,7 +48,16 @@ export default function Study(){
     }
   }
 
-  const blobby = new Blob(['little baby txt file test'], {type: 'text/plain'})
+
+  useEffect(()=>{
+    if(!scripture) return;
+    let blobby = new Blob([scripture?.verse_title + "\n" + note],{type:"text/plain"});
+    setDownloadURL(blobby);
+
+
+  },[note])
+
+  const dURL = downloadURL?window.URL.createObjectURL(downloadURL):"/";
 
   const getScriptures = async () => {
     await fetch("./book-of-mormon.json")
@@ -50,12 +66,11 @@ export default function Study(){
   }
 
   if (scripture) {
-    console.log(scripture)
     return(
-        <div className='flex flex-col h-screen w-full justify-center items-center'>
+        <div className='flex flex-col h-screen w-full justify-center items-center bg-[#F8F1EA]'>
           <Drawer.Root shouldScaleBackground>
 
-              <div className='w-[300px] flex flex-col bg-indigo-600 p-5 text-white rounded-xl shadow-2xl'>
+              <div className='w-[300px] flex flex-col bg-white p-5 text-black rounded-xl shadow-2xl'>
                 <h2 className='text-3xl font-bold mb-5'>{scripture.verse_title}</h2>
                 <p className=''>{scripture.scripture_text}</p>
                 <div className='flex flex-row w-full mt-5 justify-between'>
@@ -63,25 +78,23 @@ export default function Study(){
                       <button><AiOutlineEdit className="text-2xl"/></button>
                   </Drawer.Trigger>
                   <a href={openVerseWeb()} target='_blank'><AiOutlineInfoCircle className="text-2xl"/></a>
-                  <button onClick={()=>shareNotes()}><AiOutlineShareAlt className="text-2xl"/></button>
+                  <button className='block lg:hidden' onClick={()=>shareNotes()}><AiOutlineShareAlt className="text-2xl"/></button>
+                  <a href={dURL} download={`Celestial-${currDate}.txt`} className='hidden lg:block'><AiOutlineDownload className="text-2xl"/></a>
                 </div>
               </div>
-
-
-
               <Drawer.Overlay className="fixed inset-0 bg-black/40" />
               <Drawer.Portal className='flex md:hidden'>
-              <Drawer.Content className="bg-zinc-100 flex flex-col h-[96%] items-center mt-24 fixed bottom-0 left-0 right-0">
+              <Drawer.Content className="bg-white flex flex-col h-[96%] items-center mt-24 fixed bottom-0 left-0 right-0">
                 <div className="mx-auto my-5 w-16 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
-                      <div className='w-full h-full flex flex-col justify-center items-center'>
-                        <div className='m-5'>
+                      <div className='w-full h-full flex flex-col items-center'>
+                        <div className='m-5 flex flex-col'>
                           <div className='mb-3'>
                             <Drawer.Title className="font-bold text-2xl">
-                              Scribble Your <span className='text-indigo-600'> Thoughts </span>
+                              Scribble Your Thoughts
                             </Drawer.Title>
                             <p className='text-md text-slate-400'>How did the scripture make you feel?</p>
                           </div>
-                          <textarea className='w-[300px] h-[300px] p-3 resize-none rounded-lg border-solid border-2 border-indigo-600' />
+                          <textarea onChange={e=>setNote(e.target.value)} value={note} className='w-[300px] h-[300px] p-3 resize-none rounded-lg border-solid border-slate-400 border-2' />
                         </div>
                       </div>
               </Drawer.Content>
@@ -91,8 +104,8 @@ export default function Study(){
     )
   }
     return(
-        <div>
-            <p>loading</p>
+        <div className='bg-[#F8F1EA] h-screen w-full flex justify-center items-center'>
+          <JellyTriangle />
         </div>
     )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,37 @@ import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 const manifestForPlugin: Partial<VitePWAOptions> = {
 	registerType: "prompt",
 	includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+	manifest:{
+		"name": "Celestial",
+		"short_name": "Celestial",
+		"description": "An app to provide quick daily scripture study",
+		"icons": [
+			{
+				"src": "/android-chrome-192x192.png",
+				"sizes": "192x192",
+				"type": "image/png",
+				"purpose": "monochrome"
+			},
+			{
+				"src": "/apple-touch-icon.png",
+				"sizes": "180x180",
+				"type": "image/png",
+				"purpose": "any"
+			},
+			{
+				"src": "/maskable_icon.png",
+				"sizes": "225x225",
+				"type": "image/png",
+				"purpose": "maskable"
+			}
+		],
+		"theme_color": "#F8F1EA",
+		"background_color": "#F8F1EA",
+		"display": "standalone",
+		"scope": "/",
+		"start_url": "/",
+		"orientation": "portrait"
+	}
 };
 // https://vitejs.dev/config/
 export default defineConfig({
